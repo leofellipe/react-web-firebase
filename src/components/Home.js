02f07d0f0400e696b2d1a3dc409f82d8c91d05a7
Leonardo Fellipe <leofellipesouza@gmail.com>
2023-04-13 15:32:06 -0300

    feat: home with name and lastname registered

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import {Link} from 'react-router-dom'
+import firebase from "../database/Firebase"
+
 const Home = () => {
+  const [name, setName] = useState()
+  const [lastName, setLastName] = useState()
+  
+  firebase.auth().onAuthStateChanged(async (user) => {
+    if(user) {
+      const uid = user.uid
+
+      await firebase.firestore().collection('users').doc(uid).get()
+        .then((response) => {
+          setName(response.data().nome)
+          setLastName(response.data().sobrenome)
+        })
+    } 
+  })  
   return(
     <div>
       <h1>Home</h1>
+      <p>{name}</p>
+      <p>{lastName}</p>
+      <Link to="/"/>
     </div>
   )
 }
